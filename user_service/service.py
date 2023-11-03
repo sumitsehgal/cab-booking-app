275f74eee1ff0b80f36c0df99625b646c66a4e86
fsmadani <108167654+fsmadani@users.noreply.github.com>
2023-11-03 13:41:58 +0530

    Completed pending Service.py

--- user_service/service.py
+++ user_service/service.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request, Blueprint
+from user_service.users import Drivers
 from users import Users
 
 app = Flask("'User-Service")
@@ -17,7 +18,7 @@ def get_user(user_id):
 def add_user():
     if request.method == 'POST':
         request_data = request.get_json()
-        isUserAdded = Users().add(request_data)
+        isUserAdded = Users.get_instance().add(request_data)
         
         if isUserAdded is None:
             return jsonify({'Status':'Error', 'Message': "There is problem while registering"})
@@ -26,25 +27,28 @@ def add_user():
 
 @api_v1.route("/user/<user_id>", methods=["DELETE"])
 def delete_user(user_id):
-    Users().delete_by_id(user_id)
+    Users.get_instance().delete_by_id(user_id)
     return jsonify({'Status':"Ok"})
 
 @api_v1.route("/user/<user_id>", methods=["PATCH"])
 def update_user(user_id):
     request_data = request.get_json()
-    Users().edit(user_id, request_data)
+    Users.get_instance().edit(user_id, request_data)
     return jsonify({'Status':"Ok"})
 
 
 @api_v1.route("/driver/<driver_id>")
 def get_driver(driver_id):
-    return jsonify({'Driver':driver_id})
+    driver = Drivers.get_instance().get_by_id(driver_id)
+    return jsonify({'Status':"Ok", "Data": driver})
 
 @api_v1.route("/driver", methods=["POST"])
 def put_driver():
-    if request.method == 'POST':
-        request_data = request.get_json()
-    return jsonify({'Status':'Ok', 'Cab User' : request_data['Name']})
+    request_data = request.get_json()
+    isUserAdded = Drivers.get_instance().add(request_data)
+    if isUserAdded is None:
+        return jsonify({'Status':'Error', 'Message': "There is problem while registering"})
+    return jsonify({'Status':'Ok', 'UserId': str(isUserAdded), 'Name': request_data['first_name']})
 
 
 
